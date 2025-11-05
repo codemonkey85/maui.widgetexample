6a83d5bbaed53cc055a10378911b4dad6b7c2ac6
Toine db <toine.de.boer@enbyin.com>
2025-11-05 12:23:31 +0100

    new MAUI solution

---
+++ WidgetExample/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WidgetExample")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WidgetExample.Pages")]
