50886e5d564cd42bf14e28ae2b92bda4aaaab6af
Toine de Boer <toine.de.boer@enbyin.com>
2025-11-05 15:20:07 +0100

    added new Widget extension

---
+++ XCodeWidgetExample/MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Toine Boer on 05/11/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+    }
+}
