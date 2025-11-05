1128edab8f7cdece1a023d3d1fc7dda0b43e3f9c
Toine de Boer <toine.de.boer@enbyin.com>
2025-11-05 12:58:48 +0100

    new XCode project

---
+++ XCodeWidgetExample/XCodeWidgetExample/XCodeWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeWidgetExampleApp.swift
+//  XCodeWidgetExample
+//
+//  Created by Toine Boer on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct XCodeWidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
