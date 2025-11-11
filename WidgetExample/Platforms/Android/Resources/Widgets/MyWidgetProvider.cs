6bb032c15ec5598aa11b00e2f57789e9980b4709
Toine db <toine.de.boer@enbyin.com>
2025-11-11 10:14:14 +0100

    Android basic static widget

---
+++ WidgetExample/Platforms/Android/Resources/Widgets/MyWidgetProvider.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Appwidget;
+using Android.Content;
+using Android.Widget;
+
+namespace WidgetExample.Platforms.Android.Resources.Widgets;
+
+[BroadcastReceiver(Label = "My Widget", Exported = true)]
+[IntentFilter(new[] { AppWidgetManager.ActionAppwidgetUpdate })]
+[MetaData(AppWidgetManager.MetaDataAppwidgetProvider, Resource = "@xml/mywidget_provider_info")]
+public class MyWidgetProvider : AppWidgetProvider
+{
+	public override void OnUpdate(Context? context, AppWidgetManager? appWidgetManager, int[]? appWidgetIds)
+	{
+		if (context == null
+			|| appWidgetIds == null
+			|| appWidgetManager == null)
+		{
+			return;
+		}
+
+		// Update all widgets for specified IDs
+		foreach (var appWidgetId in appWidgetIds)
+		{
+			var views = BuildRemoteViews(context);
+			appWidgetManager.UpdateAppWidget(appWidgetId, views);
+		}
+	}
+
+	private static RemoteViews BuildRemoteViews(Context context)
+	{
+		var views = new RemoteViews(context.PackageName, Resource.Layout.mywidget_simple);
+		views.SetTextViewText(Resource.Id.widgetText, "Count: 5 (static)");
+
+		return views;
+	}
+}
